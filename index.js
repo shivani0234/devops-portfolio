c5e4371cb3d1405ce5dc845427f1623b031c1995
Shivani Kumari <shivani0234@gmail.com>
2026-04-06 00:24:40 +0530

    added CI pipeline

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("DevOps Portfolio App Running 🚀");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
